15e1a6f045bfdf82e2add52da444eb4729490bef
omartrese <omarlayachi13@gmail.com>
2023-01-14 21:06:54 +0100

    buttons_update1

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 addEventListener('DOMContentLoaded', () => {
-  const button = document.getElementById('button');
+  const buttons = document.querySelectorAll('.buttonNumber');
+  const equalTo = document.getElementById('equalTo');
+  const result = document.getElementById('result');
+  let firstValue;
+  let firstValueNum;
 
-  button.addEventListener('click', () => {
+  let secondValue;
+  let secondValueNum;
 
-    alert("hola wacho");
+  result.innerText = '';
+
+  for(let i = 0; i < buttons.length - 1; i++)
+  {
+    buttons[i].addEventListener('click', () => {
+      firstValue = buttons[i].innerHTML;
+      result.innerText += firstValue;
+    })
+  }
+
+  buttons[10].addEventListener('click', () => {
+
+    
 
   })
+
+  /* 8732485373 => int => firstValue 
+  al obtener firstValue y SecondValue con el mismo procedimiento en cada uno
+  se obtiene el tipo de operacion matematica elegida y se aplica en ambos valores
+  */
+
 })
