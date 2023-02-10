5f57083678f26d2ad87dcdd8faf36c070f77b77e
omartrese <omarlayachi13@gmail.com>
2023-02-10 18:08:48 +0100

    AC_Button

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
 addEventListener('DOMContentLoaded', () => {
   const buttons = document.querySelectorAll('.number');
+  const calcButtons = document.querySelectorAll('.operation');
   const equalTo = document.getElementById('equalTo');
   const result = document.getElementById('result');
-  
+  const deleteButton = document.getElementById('delete');
+
+  let isFirst = true;
+
+  let numberText;
+
   let firstValue;
-  let firstValueNum;
-
   let secondValue;
-  let secondValueNum;
-
-  result.innerText = '';
-
   
+    result.innerText = '0';
       
     for(let i = 0; i < buttons.length; i++)
     {
       buttons[i].addEventListener('click', () => {
-        firstValue = buttons[i].innerText;
-        if(result.innerText.length < 37)
-        {
-          result.innerText += firstValue;
-        }
+          numberText = buttons[i].innerText;
+          if(result.innerText.length < 37)
+          {
+            if(result.innerText === "0")
+            {
+              result.innerText = numberText;
+            }else
+            result.innerText += numberText;
+          }
+
       })
     }
-  
-
-  equalTo.addEventListener('click', () => {
 
     
 
-  })
-
-
-
+    deleteButton.addEventListener('click', () => {
+      numberText = "0";
+      result.innerText = numberText;
+    })
   /* 8732485373 => int => firstValue 
   al obtener firstValue y SecondValue con el mismo procedimiento en cada uno
   se obtiene el tipo de operacion matematica elegida y se aplica en ambos valores
