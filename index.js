b5bf519d969bdb19e053e85fe6ecf4f7b9c17646
omartrese <omarlayachi13@gmail.com>
2023-02-10 19:22:43 +0100

    calculator logic

--- index.js
+++ index.js
@@ -2,43 +2,104 @@ addEventListener('DOMContentLoaded', () => {
   const buttons = document.querySelectorAll('.number');
   const calcButtons = document.querySelectorAll('.operation');
   const equalTo = document.getElementById('equalTo');
-  const result = document.getElementById('result');
+  const resultText = document.getElementById('result');
   const deleteButton = document.getElementById('delete');
 
   let isFirst = true;
 
-  let numberText;
+  let strFirstValue;
+  let strSecondValue;
+  let strCalc;
 
   let firstValue;
   let secondValue;
+  let result;
   
-    result.innerText = '0';
+  resultText.innerText = '0';
       
+  inputValues(strFirstValue);
+
+  strCalc = inputOperations();
+
+  inputValues(strSecondValue);
+
+  switch (strCalc) 
+  {
+    case "+":
+      result = parseInt(strFirstValue) + parseInt(strSecondValue);
+      break;
+  
+    case "-":
+      result = parseInt(strFirstValue) - parseInt(strSecondValue);
+      break;
+
+    case "x":
+      result = parseInt(strFirstValue) * parseInt(strSecondValue);
+      break;
+
+    case "/":
+      result = parseInt(strFirstValue) / parseInt(strSecondValue);
+      break;
+
+    default:
+      result = parseInt(strFirstValue);
+      break;
+  }
+
+  equalTo.addEventListener('click', () => {
+      resultText = result.toString();
+      numberText = "";
+  })
+
+  deleteButton.addEventListener('click', () => {
+      numberText = "0";
+      resultText.innerText = numberText;
+  })
+
+
+
+  function inputValues(x)
+  {
     for(let i = 0; i < buttons.length; i++)
     {
-      buttons[i].addEventListener('click', () => {
-          numberText = buttons[i].innerText;
-          if(result.innerText.length < 37)
+      buttons[i].addEventListener('click', () => 
+      {
+          x = buttons[i].innerText;
+          if(resultText.innerText.length < 37)
           {
-            if(result.innerText === "0")
+            if(resultText.innerText === "0")
             {
-              result.innerText = numberText;
+              resultText.innerText = x;
             }else
-            result.innerText += numberText;
+            resultText.innerText += x;
           }
 
       })
     }
+  }
+  
+  function inputOperations() 
+  {
+    let operation;
 
-    
+    for(let i = 0; i < calcButtons.length; i++)
+    {
+      calcButtons[i].addEventListener('click', () =>
+      {
+        isFirst = false;
+        inputValues(strSecondValue);
 
-    deleteButton.addEventListener('click', () => {
-      numberText = "0";
-      result.innerText = numberText;
-    })
+        operation = calcButtons[i].innerText;
+
+        return operation;
+      })
+    }  
+  }
+
+})
+
+
   /* 8732485373 => int => firstValue 
   al obtener firstValue y SecondValue con el mismo procedimiento en cada uno
   se obtiene el tipo de operacion matematica elegida y se aplica en ambos valores
   */
-
-})
